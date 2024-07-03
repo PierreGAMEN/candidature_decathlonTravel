257bd7b134c2ee9e64a332d8257d7df85f0fe5e1
Pierre GAMEN <pierre.gamen@gmail.com>
2024-07-03 08:03:30 +0200

    add: sport to Pierre GAMEN profil

--- src/dataDev.js
+++ src/dataDev.js
@@ -2,7 +2,7 @@ const dataDev = [
     {
         name : "Pierre GAMEN",
         disponibility: new Date ("2024-08-01"),
-        sports: ["escalade", "trail", "ski de fond", "ski alpin", "ski de rando"],
+        sports: ["escalade", "trail", "ski de fond", "ski alpin", "ski de rando", "randonnée", "trekking"],
         place: "Annecy",
         img: "https://pierre-gamen.netlify.app/photoPg.png",
         techno : ["HTML, CSS, Javascript, React, Node.js, Express, GraphQL"]
